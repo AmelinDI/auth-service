47ce276a14795cf1b13914f90dd446d2b4c38f09
Тренкунов Антон Валентинович <17238535@sigma.sbrf.ru>
2021-06-08 19:54:30 +0300

    d'

--- src/main/java/ru/reboot/dto/User.java
+++ src/main/java/ru/reboot/dto/User.java
@@ -1,15 +1,4 @@
 package ru.reboot.dto;
 
 public class User {
-
-    private String id;
-    private String name;
-    private String shortName;
-    private String description;
-    private String unit;
-    private double purchasePrice;
-    private String category;
-    private String manufacturer;
-    private int count;
-    private int reserved;
 }
