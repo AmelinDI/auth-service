ecadef17baa5658b91da7c5d9877f23995aa8e44
Тренкунов Антон Валентинович <17238535@sigma.sbrf.ru>
2021-06-08 09:07:09 +0300

    scenatio

--- src/main/java/ru/reboot/service/AuthService.java
+++ src/main/java/ru/reboot/service/AuthService.java
@@ -9,36 +9,55 @@ public interface AuthService {
 
     /**
      * Получить информацию о пользователе по userId
+     *
+     * @throws ru.reboot.error.BusinessLogicException with code USER_NOT_FOUND if user doesn't exist
+     * @throws ru.reboot.error.BusinessLogicException with code ILLEGAL_ARGUMENT if userId is empty or null
      */
     User getUserByUserId(String userId);
 
     /**
      * Получить информацию о пользователе по login
+     *
+     * @throws ru.reboot.error.BusinessLogicException with code USER_NOT_FOUND if user doesn't exist
+     * @throws ru.reboot.error.BusinessLogicException with code ILLEGAL_ARGUMENT if login is empty or null
      */
     User getUserByLogin(String login);
 
     /**
      * Удалить информацию о пользователе
+     *
+     * @throws ru.reboot.error.BusinessLogicException with code USER_NOT_FOUND if user doesn't exist
+     * @throws ru.reboot.error.BusinessLogicException with code ILLEGAL_ARGUMENT if userId is empty or null
      */
     void deleteUser(String userId);
 
     /**
      * Создать нового пользователя
+     *
+     * @throws ru.reboot.error.BusinessLogicException with code USER_ALREADY_EXISTS if user with id exists
+     * @throws ru.reboot.error.BusinessLogicException with code DUPLICATE_LOGIN if user with login exists
+     * @throws ru.reboot.error.BusinessLogicException with code ILLEGAL_ARGUMENT if request contains wrong data
      */
     User createUser(User user);
 
     /**
      * Обновить информацию о существующем пользователе
+     *
+     * @throws ru.reboot.error.BusinessLogicException with code USER_NOT_FOUND if user doesn't exist
+     * @throws ru.reboot.error.BusinessLogicException with code DUPLICATE_LOGIN if user with login exists
+     * @throws ru.reboot.error.BusinessLogicException with code ILLEGAL_ARGUMENT if request contains wrong data
      */
     User updateUser(User user);
 
     /**
-     * Получить все товары
+     * Получить всех пользователей
      */
     List<User> getAllUsers();
 
     /**
      * Получить всех пользователей по данным ролям
+     *
+     * @throws ru.reboot.error.BusinessLogicException with code ILLEGAL_ARGUMENT if roles is null
      */
     List<String> getAllUsersByRole(Collection<String> roles);
 }
